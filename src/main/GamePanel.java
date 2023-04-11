55332906cb54d467dc0b6a16ac0766ec8c22c57e
MarkSon-42 <mark42son@gmail.com>
2023-04-11 12:58:34 +0900

    java OOAD GAME - 2

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,6 +14,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
     public GamePanel() {
@@ -21,6 +22,7 @@ public class GamePanel extends JPanel implements Runnable{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
     }
 
     public void startGameThread() {
@@ -35,7 +37,26 @@ public class GamePanel extends JPanel implements Runnable{
 
 //            System.out.println("The game loop is running");
 
-            // 1. UPDATE : update ..
+            // 1. UPDATE : update ..info such as character positions
+            update();
+            // 2. DRAW : draw the screen update information
+            repaint();
         }
     }
+    public void update() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
+    }
+
 }
