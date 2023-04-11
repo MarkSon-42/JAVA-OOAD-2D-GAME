45726d19297aa62dadcd35245e3ce809d422bcd4
MarkSon-42 <mark42son@gmail.com>
2023-04-11 14:18:41 +0900

    2nd done

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -38,34 +38,73 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
 
     }
+//    @Override
+//    public void run() {
+//
+//        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//        while(gameThread != null) {
+//
+//
+//            // 1. UPDATE : update ..info such as character positions
+//            update();
+//            // 2. DRAW : draw the screen update information
+//            repaint();
+//
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime/1000000;
+//
+//                if (remainingTime < 0) {
+//
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+
+
     @Override
     public void run() {
 
-        double drawInterval = 1000000000/FPS; // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
         while(gameThread != null) {
 
+            currentTime = System.nanoTime();
 
-            // 1. UPDATE : update ..info such as character positions
-            update();
-            // 2. DRAW : draw the screen update information
-            repaint();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta --;
+                drawCount++;
+            }
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime/1000000;
+            if(timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
 
-                if (remainingTime < 0) {
-
-                }
-
-                Thread.sleep((long) remainingTime);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             }
         }
     }
+
     public void update() {
         if(keyH.upPressed == true) {
             playerY -= playerSpeed;
