210c7c2418f83e4214102b70b8cc005349d48f83
minwoo <mark42son@gmail.com>
2023-06-05 12:10:18 +0900

    restart ooad prj

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setDeYfaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure OOAD");
 
